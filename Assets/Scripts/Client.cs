36161425bb2cc9300976341f97ad6ba48b6946d8
mephistia <39158254+mephistia@users.noreply.github.com>
2020-11-03 17:23:46 -0300

    Handling disconnections

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -20,7 +20,7 @@ public class Client : MonoBehaviour
     public TCP tcp; 
     public UDP udp;
 
-
+    private bool isConnected = false;
     private delegate void PacketHandler(Packet _packet);
     private static Dictionary<int, PacketHandler> packetHandlers;
 
@@ -45,10 +45,19 @@ public class Client : MonoBehaviour
         udp = new UDP();
     }
 
+    // para desconectar no editor:
+    private void OnApplicationQuit()
+    {
+        Disconnect();
+    }
+
+
+
     public void ConnectToServer()
     {
         InitializeClientData();
 
+        isConnected = true;
         tcp.Connect();
 
     }
@@ -113,7 +122,7 @@ public class Client : MonoBehaviour
                 int _byteLength = stream.EndRead(_result);
                 if (_byteLength <= 0)
                 {
-                    // desconectar
+                    instance.Disconnect();
                     return;
                 }
 
@@ -130,7 +139,7 @@ public class Client : MonoBehaviour
             catch (Exception _ex)
             {
                 Console.WriteLine($"Error receiving TCP data: {_ex} ");
-                // desconectar
+                Disconnect();
             }
         }
 
@@ -180,6 +189,16 @@ public class Client : MonoBehaviour
             }
 
             return false;
+        }
+
+        private void Disconnect()
+        {
+            instance.Disconnect();
+            stream = null;
+            receivedData = null;
+            receiveBuffer = null;
+            socket = null;
+
         }
     }
 
@@ -233,7 +252,7 @@ public class Client : MonoBehaviour
 
                 if (_data.Length < 4)
                 {
-                    // desconectar?
+                    instance.Disconnect();
                     return;
                 }
 
@@ -241,7 +260,7 @@ public class Client : MonoBehaviour
             }
             catch
             {
-                // desconectar??
+                Disconnect();
             }
         }
 
@@ -264,6 +283,13 @@ public class Client : MonoBehaviour
            });
         }
 
+        private void Disconnect()
+        {
+            instance.Disconnect();
+            endPoint = null;
+            socket = null;
+        }
+
 
     }
 
@@ -278,6 +304,18 @@ public class Client : MonoBehaviour
         };
         Debug.Log("Initialized packets.");
 
+    }
+
+    private void Disconnect()
+    {
+        if (isConnected)
+        {
+            isConnected = false;
+            tcp.socket.Close();
+            udp.socket.Close();
+
+            Debug.Log("Disconnected from server.");
+        }
     }
 
 }
